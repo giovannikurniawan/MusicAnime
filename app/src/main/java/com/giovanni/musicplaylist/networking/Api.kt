ac8d082a25604ca254bce1da7868833bbc122fa5
Giovanni Kurniawan <giovanni.kurniawan@student.umn.ac.id>
2021-05-17 09:38:19 +0700

    Maaf Telat Upload

---
+++ app/src/main/java/com/giovanni/musicplaylist/networking/Api.kt
@@ -0,0 +1,10 @@
+package com.giovanni.musicplaylist.networking
+
+/**
+ * Created by Azhar Rivaldi on 22-12-2019.
+ */
+
+object Api {
+    var ListMusic = "https://animemusic.us/listmusic"
+    var DetailMusic = "https://animemusic.us/jsongetid/{id}"
+}
